f107435f4f6ef4acadac6b7b6a8a8e0a71da2c69
Sakib <najmuskg@gmail.com>
2026-05-19 16:55:58 +0600

    set up

---
+++ markup/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html class="loading" lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="" rel="stylesheet">
+  <title>Golpo Land</title>
+</head>
+
+<body>
+
+  <header class="fixed w-full left-0 top-0 z-999 lg:bg-transparent py-6 md:py-8 transition-all duration-200">
+    <div class="holder">
+      <div class="menu-wrapper flex items-center justify-between">
+        <h1 class="siteLogo z-10" title="Golpo Land">
+          <a href="index.php" class="block">
+            <img src="/wp-content/themes/golpoLand/_/images/Logo_en.svg" alt="">
+          </a>
+        </h1><!-- header logo -->
+
+        <nav class="lg:block nav--menu" mobile-view="false">
+          <ul class="main--menu flex flex-col lg:flex-row flex-wrap gap-10 xl:gap-22">
+            <li><a href="#">Tab 1</a></li>
+            <li><a href="#">Tab 2</a></li>
+            <li><a href="#">Tab 3</a></li>
+            <li><a href="#">Tab 4</a></li>
+          </ul>
+        </nav><!-- main menu -->
+
+      </div>
+    </div>
+  </header>
+
+
+
+  <main class="flex-auto">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </main>
+
+
+  <!-- <script type='text/javascript' src='/wp-includes/js/jquery/jquery.min.js?ver=3.6.0'></script>
+  <script type='text/javascript' src='/wp-content/themes/websureChelseaTech/_/js/functions.js?ver=<?php echo uniqid(); ?>'></script> -->
+</body>
+
+</html>
